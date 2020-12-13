6515010e2ae42e2627f989fba46a13be61a27535
Asraou Ismail <asraou.ismail@gmail.com>
2020-12-13 12:06:33 +0100

    modifications

---
+++ src/components/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+ return (
+     <div className='col col-sm-4'>
+         <input className='form-control' placeholder="Type to search" type="text"/>
+     </div>
+ )
+}
+export default SearchBox;
